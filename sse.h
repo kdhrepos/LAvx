2b7d45b63d96caa883397ffdd26b3637ac88eef5
kdhrepos <tmdrl583205@naver.com>
2024-11-13 23:02:16 +0900

    simd.h -> sse.h

--- sse.h
+++ sse.h
@@ -9,30 +9,30 @@
 #elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
      /* GCC-compatible compiler, targeting x86/x86-64 */
      #include <x86intrin.h>
-#elif defined(__GNUC__) && defined(__ARM_NEON__)
-     /* GCC-compatible compiler, targeting ARM with NEON */
-     #include <arm_neon.h>
-#elif defined(__GNUC__) && defined(__IWMMXT__)
-     /* GCC-compatible compiler, targeting ARM with WMMX */
-     #include <mmintrin.h>
-#elif (defined(__GNUC__) || defined(__xlC__)) && (defined(__VEC__) || defined(__ALTIVEC__))
-     /* XLC or GCC-compatible compiler, targeting PowerPC with VMX/VSX */
-     #include <altivec.h>
-#elif defined(__GNUC__) && defined(__SPE__)
-     /* GCC-compatible compiler, targeting PowerPC with SPE */
-     #include <spe.h>
+// #elif defined(__GNUC__) && defined(__ARM_NEON__)
+//      /* GCC-compatible compiler, targeting ARM with NEON */
+//      #include <arm_neon.h>
+// #elif defined(__GNUC__) && defined(__IWMMXT__)
+//      /* GCC-compatible compiler, targeting ARM with WMMX */
+     // #include <mmintrin.h>
+// #elif (defined(__GNUC__) || defined(__xlC__)) && (defined(__VEC__) || defined(__ALTIVEC__))
+//      /* XLC or GCC-compatible compiler, targeting PowerPC with VMX/VSX */
+//      #include <altivec.h>
+// #elif defined(__GNUC__) && defined(__SPE__)
+//      /* GCC-compatible compiler, targeting PowerPC with SPE */
+//      #include <spe.h>
 #endif
 
 // #if defined (__AVX512VL__)
 // #if defined (__AVX512F__) || defined ( __AVX512__ )
 // #if defined ( __AVX512VNNI__ )
 
+// TODO: Define another macros (or not...?)
 #ifndef INSTLEVEL
 
-// #if defined (__AVX2__)
-// #define INSTLEVEL 7
-// #elif defined (__AVX__)
-#if defined (__AVX__)
+#if defined (__AVX2__)
+#define INSTLEVEL 7
+#elif defined (__AVX__)
 #define INSTLEVEL 6
 #elif defined (__SSE4_2__)
 #define INSTLEVEL 5
